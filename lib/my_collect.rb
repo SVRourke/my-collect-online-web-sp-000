5d143d9ce8eb38f46b654fa0b17850bf1bdc03e3
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-14 02:43:14 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,8 @@ def my_collect(array)
   count = 0
   collection = []
   while count < array.count
-    collection << yield(array[i])
+    collection << yield(array[count])
+    count += 1
   end
   collection
 end
